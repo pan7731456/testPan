eaf1ddc31769017a17faae08819d0dfcf578f790
pan <691229251@qq.com>
2020-04-16 17:45:33 +0800

    查找 先提交

---
+++ src/main/java/Search/BinarySearch.java
@@ -0,0 +1,69 @@
+package Search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author pan
+ * @Date 2020/4/16 16:30
+ * @Description 二分查找
+ * @Version 1.0
+ **/
+public class BinarySearch {
+    /**
+     * 二分查找只针对有序数组
+     */
+    public static List<Integer> binarySearch(int[] values, int value) {
+        return binarySearch(values, 0, values.length - 1, value);
+    }
+
+    /**
+     * 二分查找，使用递归
+     *
+     * @param values 原数组
+     * @param left   左索引
+     * @param right  右索引
+     * @param value  寻找的值
+     * @return 查找到的下标，否则返回-1
+     */
+    private static List<Integer> binarySearch(int[] values, int left, int right, int value) {
+        //说明没找到
+        if (left > right) {
+            return new ArrayList<>();
+        }
+        int mid = (left + right) / 2;
+        int midVal = values[mid]; //中间值
+        //说明比中间值要小
+        if (value < midVal) {
+            return binarySearch(values, left, mid - 1, value);
+        } else if (value < midVal) {
+            return binarySearch(values, mid + 1, right, value);
+        } else {
+            List<Integer> result = new ArrayList<>();
+            //说明找到了，这时候，需要去判断是否有多个值，这时候分不清是左边相等，还是右边相等
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || values[temp] != value) {
+                    break;
+                }
+                //否则，就temp 放入到 resIndexlist
+                result.add(temp);
+                temp--;
+            }
+            result.add(mid);
+
+            //向mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+            temp = mid + 1;
+            while (true) {
+                if (temp > values.length - 1 || values[temp] != value) {
+                    break;
+                }
+                //否则，就temp 放入到 resIndexlist
+                result.add(temp);
+                temp++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/main/java/Search/Impl.java
@@ -0,0 +1,41 @@
+package Search;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static Search.BinarySearch.binarySearch;
+import static Sort.RedixSort.redixSort;
+import static Sort.ValuesArray.valuesArray;
+
+/**
+ * @Author pan
+ * @Date 2020/4/16 17:38
+ * @Description 查找测试类
+ * @Version 1.0
+ **/
+public class Impl {
+    public static void main(String[] args) {
+        int[] valuesArray = valuesArray(20);
+        //int[] valuesArray = {4,4,4,4,3,1,1,1,1};
+        System.out.println("排序前——————");
+        System.out.println(Arrays.toString(valuesArray));
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+        redixSort(valuesArray);
+        System.out.println("排序后——————");
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序后的时间是=" + date2Str);
+        System.out.println(Arrays.toString(valuesArray));
+
+        //查找数据
+        int value = valuesArray[3];
+
+        System.out.println("查找" + value + "所在的位置:" + binarySearch(valuesArray, value));
+
+    }
+}
