36f50e6d17deb7574881a413ee429662167df217
pan <691229251@qq.com>
2020-04-26 14:31:02 +0800

    线程池创建理解

---
+++ src/main/java/ThreadPool/Impl.java
@@ -0,0 +1,24 @@
+package ThreadPool;
+
+import java.util.concurrent.*;
+
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+/**
+ * @Author pan
+ * @Date 2020/4/26 14:09
+ * @Description
+ * @Version 1.0
+ **/
+public class Impl {
+
+    public static void main(String[] args) {
+
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
+//        ExecutorService s1 = Executors.newSingleThreadExecutor();
+//        ExecutorService s2 = Executors.newCachedThreadPool();
+//        Executors.defaultThreadFactory();
+//        RejectedExecutionHandler defaultHandler = new ThreadPoolExecutor.AbortPolicy(); //拒绝策略
+
+    }
+}
