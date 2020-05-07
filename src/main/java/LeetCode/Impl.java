bf41f3abd7f25f71bcb9bfa82ae2cd57dc72e3e1
pan <691229251@qq.com>
2020-05-07 09:42:09 +0800

    最低票价理解

--- src/main/java/LeetCode/Impl.java
+++ src/main/java/LeetCode/Impl.java
@@ -15,11 +15,12 @@ public class Impl {
 //        int[] costs = {2, 7, 15};
 
 //        lowestFare.mincostTickets(days, costs);
-        Solution solution = new Solution();
+//        Solution solution = new Solution();
+        LowestFare_ low = new LowestFare_();
 
         int[] days = {1, 2, 3, 4, 6, 8, 9, 10, 13, 14, 16, 17, 19, 21, 24, 26, 27, 28, 29};
         int[] costs = {3, 14, 50};
-        System.out.println(solution.mincostTickets(days, costs));
+        System.out.println(low.mincostTickets(days, costs));
 
 
     }
---
+++ src/main/java/LeetCode/LowestFare_.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * @Author pan
+ * @Date 2020/5/6 18:00
+ * @Description
+ * @Version 1.0
+ **/
+public class LowestFare_ {
+
+    public int mincostTickets(int[] days, int[] costs) {
+        //用来记录原数组中的下标
+        int index = 0;
+        //用于记录每天的最低花费，其中0是填充位
+        int[] fare = new int[days[days.length - 1] + 1];
+        for (int today = 1; today < fare.length; today++) {
+
+            if (today != days[index]) {
+                //不旅行,由于存在 0填充位 因此不需要校验
+                fare[today] = fare[today - 1];
+                continue;
+            }
+            //旅行
+            index++;
+
+            fare[today] = Math.min(Math.min(
+                    fare[today - 1] + costs[0],
+                    fare[Math.max(0, today - 7)] + costs[1]),
+                    fare[Math.max(0, today - 30)] + costs[2]
+            );
+        }
+
+        //返回最后一天的最小花费
+        return fare[fare.length - 1];
+    }
+}
